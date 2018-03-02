76610c8478ece31d2394ede7b4028f6abddacf53
aschaefer <andre.schaefer@namics.com>
2018-03-02 15:55:18 +0100

    New CI / CD Process to optimize workflow

---
+++ ci/mvn-release.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+
+	CURRENT_VERSION=`bash ci/current-version.sh`
+
+	if [[ $CURRENT_VERSION == *-SNAPSHOT ]]; then
+		NEW_VERSION=${CURRENT_VERSION%'-SNAPSHOT'}
+		NEXT_VERSION=`bash ci/semver.sh -p $NEW_VERSION`
+		NEXT_SNAPSHOT="$NEXT_VERSION-SNAPSHOT"
+		echo "perform releas of $NEW_VERSION from $CURRENT_VERSION and set next develop version $NEXT_SNAPSHOT"
+
+		mvn versions:set -DnewVersion=$NEW_VERSION
+		mvn versions:commit
+
+        echo "commit new release version"
+		git commit -a -m "Release $NEW_VERSION: set master to new release version"
+
+		echo "Update version in README.md"
+		sed -i -e 's|<version>[0-9A-Za-z._-]\{1,\}</version>|<version>TEST</version>|g' README.md && rm -f README.md-e
+		git commit -a -m "Release $NEW_VERSION: Update README.md"
+
+		echo "Build maven site"
+		source ci/mvn-site.sh
+
+		echo "create tag for new release"
+		git tag -a $NEW_VERSION -m "Release $NEW_VERSION: tag release"
+
+		echo "merge master back to develop"
+		git checkout develop
+		git merge master
+
+		mvn versions:set -DnewVersion=$NEXT_SNAPSHOT
+		mvn versions:commit
+
+		echo "commit new snapshot version"
+		git commit -a -m "Release $NEW_VERSION: set develop to next development version $NEXT_SNAPSHOT"
+
+		git push github --all
+		git push github --tags
+
+	fi
+fi
---
+++ ci/mvn-site.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+
+	mvn clean site:site site -DtestFailureIgnore=true --fail-never -DskipITs=false -P site --settings ci/mvnsettings.xml
+	mvn site:stage -DtestFailureIgnore=true --fail-never  -P site
+
+	echo "Clean old docs folder"
+	rm -rf docs/*
+
+	echo "Deploy new site"
+	mvn site:deploy -DtestFailureIgnore=true --fail-never  -P site
+
+	git add docs
+
+	CURRENT_VERSION=`bash ci/current-version.sh`
+	git commit -a -m "Version $CURRENT_VERSION: Update docs"
+	git push github --force
+fi
