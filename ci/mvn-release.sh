259da007d8a27f9cf7054aaf5e5a11c1e282bc69
aschaefer <andre.schaefer@namics.com>
2021-06-02 15:06:56 +0200

    try to switch to github actions for releases

--- ci/mvn-release.sh
+++ ci/mvn-release.sh
@@ -1,41 +1,39 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	CURRENT_VERSION=`xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' pom.xml`
+CURRENT_VERSION=`xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' pom.xml`
 
-	if [[ $CURRENT_VERSION == *-SNAPSHOT ]]; then
-		NEW_VERSION=${CURRENT_VERSION%'-SNAPSHOT'}
-		NEXT_VERSION=`bash ci/semver.sh -p $NEW_VERSION`
-		NEXT_SNAPSHOT="$NEXT_VERSION-SNAPSHOT"
-		echo "perform release of $NEW_VERSION from $CURRENT_VERSION and set next develop version $NEXT_SNAPSHOT"
+if [[ $CURRENT_VERSION == *-SNAPSHOT ]]; then
+  NEW_VERSION=${CURRENT_VERSION%'-SNAPSHOT'}
+  NEXT_VERSION=`bash ci/semver.sh -p $NEW_VERSION`
+  NEXT_SNAPSHOT="$NEXT_VERSION-SNAPSHOT"
+  echo "perform release of $NEW_VERSION from $CURRENT_VERSION and set next develop version $NEXT_SNAPSHOT"
 
-		mvn versions:set -DnewVersion=$NEW_VERSION
-		mvn versions:commit
+  mvn versions:set -DnewVersion=$NEW_VERSION
+  mvn versions:commit
 
-        echo "commit new release version"
-		git commit -a -m "Release $NEW_VERSION: set master to new release version"
+      echo "commit new release version"
+  git commit -a -m "Release $NEW_VERSION: set master to new release version"
 
-		echo "Update version in README.md"
-		sed -i -e "s|<version>[0-9A-Za-z._-]\{1,\}</version>|<version>$NEW_VERSION</version>|g" README.md && rm -f README.md-e
-		git commit -a -m "Release $NEW_VERSION: Update README.md"
+  echo "Update version in README.md"
+  sed -i -e "s|<version>[0-9A-Za-z._-]\{1,\}</version>|<version>$NEW_VERSION</version>|g" README.md && rm -f README.md-e
+  git commit -a -m "Release $NEW_VERSION: Update README.md"
 
-		echo "Build maven site"
-		source ci/mvn-site.sh
+  echo "Build maven site"
+  source ci/mvn-site.sh
 
-		echo "create tag for new release"
-		git tag -a $NEW_VERSION -m "Release $NEW_VERSION: tag release"
+  echo "create tag for new release"
+  git tag -a "v$NEW_VERSION" -m "Release $NEW_VERSION: tag release"
 
-		echo "merge master back to develop"
-		git checkout -f -b develop github/develop
-		git merge master
+  echo "merge master back to develop"
+  git checkout -f -b develop github/develop
+  git merge master
 
-		mvn versions:set -DnewVersion=$NEXT_SNAPSHOT
-		mvn versions:commit
+  mvn versions:set -DnewVersion=$NEXT_SNAPSHOT
+  mvn versions:commit
 
-		echo "commit new snapshot version"
-		git commit -a -m "Release $NEW_VERSION: set develop to next development version $NEXT_SNAPSHOT"
+  echo "commit new snapshot version"
+  git commit -a -m "Release $NEW_VERSION: set develop to next development version $NEXT_SNAPSHOT"
 
-		git push github --all
-		git push github --tags
+  git push github --all
+  git push github --tags
 
-	fi
 fi
--- ci/mvn-site.sh
+++ ci/mvn-site.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
-	mvn clean site:site site -DtestFailureIgnore=true --fail-never -DskipITs=false -P site --settings ci/mvnsettings.xml
-	mvn site:stage -DtestFailureIgnore=true --fail-never  -P site
+mvn clean site:site site -DtestFailureIgnore=true --fail-never -DskipITs=false -P site --settings ci/mvnsettings.xml
+mvn site:stage -DtestFailureIgnore=true --fail-never  -P site
 
-	echo "Clean old docs folder"
-	rm -rf docs/*
+echo "Clean old docs folder"
+rm -rf docs/*
 
-	echo "Deploy new site"
-	mvn site:deploy -DtestFailureIgnore=true --fail-never  -P site
+echo "Deploy new site"
+mvn site:deploy -DtestFailureIgnore=true --fail-never  -P site
 
-	git add docs
+git add docs
 
-	git commit -a -m "Update docs"
-fi
+git commit -a -m "Update docs"
